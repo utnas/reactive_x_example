bc0d40e76f4d51262bb1c77698942f3cc38eb4bc
Dooley <nsewolo@gmail.com>
2016-01-30 21:19:35 -0500

    Fisrt commit

---
+++ java-example/src/test/java/ObservableTest.java
@@ -0,0 +1,55 @@
+import org.junit.Test;
+import rx.Observable;
+import rx.observers.TestSubscriber;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.EMPTY_LIST;
+
+public class ObservableTest {
+
+    @Test
+    public void emptyObservable() {
+        //Given
+        final Observable<String> empty = Observable.empty();
+        final TestSubscriber<String> testSubscriber = new TestSubscriber<>();
+
+        //When
+        empty.subscribe(testSubscriber);
+
+        //Then
+        testSubscriber.assertNoErrors();
+        testSubscriber.assertReceivedOnNext(EMPTY_LIST);
+        testSubscriber.assertCompleted();
+    }
+
+    @Test
+    public void justObservableWithValue() {
+        // Given
+        final Observable<String> stringValue = Observable.just("String value");
+        final TestSubscriber<String> testSubscriber = new TestSubscriber<>();
+
+        //When
+        stringValue.subscribe(testSubscriber);
+
+        //Then
+        testSubscriber.assertNoErrors();
+        testSubscriber.assertReceivedOnNext(asList("String value"));
+        testSubscriber.assertCompleted();
+    }
+
+    @Test
+    public void fromObservable() {
+        //Given
+        final Observable<String> values = Observable.from(asList("one", "two", "three"));
+        final TestSubscriber<String> testSubscriber = new TestSubscriber<>();
+
+        //When
+        values.subscribe(testSubscriber);
+
+        //Then
+        testSubscriber.assertNoErrors();
+        testSubscriber.assertReceivedOnNext(asList("one", "two", "three"));
+        testSubscriber.assertCompleted();
+    }
+
+}
